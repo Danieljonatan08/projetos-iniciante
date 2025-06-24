b8337e05bff83796186896b4c4ab4cd68026b424
Daniel Jonatan <100439702+Danieljonatan08@users.noreply.github.com>
2025-06-23 21:49:59 -0300

    html Inicial/css/js

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,4 +1,34 @@
 fetch('https://pokeapi.co/api/v2/pokemon?limit=10')    
     .then(response => response.json())
-    .then(allpokemon => console.log(allpokemon))
+    .then(allpokemon => {
+
+        var pokemons = [];
+
+        allpokemon.results.map((val) =>{
+            
+
+            fetch(val.url)
+            .then(response => response.json())
+            .then(pokemonSingle =>{
+                pokemons.push({nome:val.name,imagem:pokemonSingle.sprites.front_default});
+                
+
+                if(pokemons.length == 10){
+                    //finalizando nossas requisições
+                    console.log(pokemons);
+                    pokemons.map(function(val){
+                        console.log(val);
+                    })
+                }
+      
+            })
+
+
+        })
+
+        
+
     
+    })
+
+
